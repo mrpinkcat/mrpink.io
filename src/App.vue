480aca359a8c772271265b3b485d3d0feb54e011
mrpinkcat <gatien.anizan@gmail.com>
2018-09-13 11:03:26 +0200

    component separation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div class="navbar">
+    <!-- <div class="navbar">
       <div class="nav-left">
         <router-link to="/" tag='div' class="title">
           MrPink.io
@@ -19,143 +19,159 @@
           <i class="fab fa-github-alt fa-lg"></i>
         </a>
       </div>
-    </div>
+    </div> -->
+    <Navbar/>
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Navbar from '@/components/Navbar.vue';
+
+@Component({
+  components: {
+    Navbar,
+  },
+})
+export default class Main extends Vue {
+  @Prop() private info!: any;
+}
+</script>
+
 
 <style lang="scss">
 @import './sass/mrpink.io';
 
 $nav-height: 50px;
 
-#app {
-  .navbar {
-    user-select: none;
-    display: flex;
-    align-items: center;
-    flex-wrap: nowrap;
-    justify-content: space-between;
-    margin: 8px;
-    padding: 0 35px;
-    height: $nav-height;
-    background: #e5e9ea;
-    border-radius: 6px;
-    color: $text-dark;
-    transition-duration: .2s;
-    position: fixed;
-    top: 0;
-    width: calc(100% - (8px * 2) - (35px * 2));
-    .nav-left {
-      display: flex;
-      align-items: center;
-      .title {
-        font-size: 1.5rem;
-        font-weight: 700;
-        cursor: pointer;
-        color: $text-pink;
-        svg {
-          margin-left: 10px;
-          transition-duration: .2s;
-        }
-        &:hover {
-          svg {
-            transform: scale(1.75) rotate(15deg);
-          }
-        }
-        &.router-link-exact-active {
-          svg {
-            transform: scale(1.75);
-          }
-        }
-      }
-    }
-    .nav-right {
-      display: flex;
-      align-items: center;
-      button {
-        height: $nav-height - 20px;
-        font-weight: 700;
-        padding: 0 10px;
-        font-family: $font;
-        background-color: transparent;
-        transition-duration: .2s;
-        &:hover {
-          transform: scale(1.15);
-          color: $text-pink;
-        }
-        &.router-link-exact-active {
-          transform: scale(1.15);
-          color: $text-pink;
-        }
-      }
-      .icon {
-        height: $nav-height - 20px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        transition-duration: .2s;
-        min-width: 40px;
-        &:hover {
-          transform: scale(1.15) rotate(15deg);
-          color: $text-pink;
-        }
-        &:visited {
-          color: $text-dark;
-          &:hover {
-            color: $text-pink;
-          }
-        }
-      }
-    }
-    @media (max-width: $breakpoint-small) {
-      flex-direction: column;
-      height: auto;
-      justify-content: space-around;
-      padding: 0;
-      width: calc(100% - (8px * 2));
-      .nav-left {
-        height: 50px;
-        .title {
-          &:hover {
-            svg {
-              transform: none;
-            }
-          }
-          &.router-link-exact-active {
-            svg {
-              transform: scale(1.75) rotate(15deg);
-            }
-          }
-        }
-      }
-      .nav-right {
-        max-width: 95%;
-        overflow-x: auto;
-        button {
-          &:hover {
-            transform: none;
-            color: $text-dark;
-          }
-          &.router-link-active {
-            transform: scale(1.15);
-            color: $text-pink;
-          }
-        }
-        .icon {
-          &:hover {
-            transform: none;
-            color: $text-dark;
-          }
-          &:visited {
-            color: $text-dark;
-            &:hover {
-              color: $text-pink;
-            }
-          }
-        }
-      }
-    }
-  }
-}
+// #app {
+//   .navbar {
+//     user-select: none;
+//     display: flex;
+//     align-items: center;
+//     flex-wrap: nowrap;
+//     justify-content: space-between;
+//     margin: 8px;
+//     padding: 0 35px;
+//     height: $nav-height;
+//     background: #e5e9ea;
+//     border-radius: 6px;
+//     color: $text-dark;
+//     transition-duration: .2s;
+//     position: fixed;
+//     top: 0;
+//     width: calc(100% - (8px * 2) - (35px * 2));
+//     .nav-left {
+//       display: flex;
+//       align-items: center;
+//       .title {
+//         font-size: 1.5rem;
+//         font-weight: 700;
+//         cursor: pointer;
+//         color: $text-pink;
+//         svg {
+//           margin-left: 10px;
+//           transition-duration: .2s;
+//         }
+//         &:hover {
+//           svg {
+//             transform: scale(1.75) rotate(15deg);
+//           }
+//         }
+//         &.router-link-exact-active {
+//           svg {
+//             transform: scale(1.75);
+//           }
+//         }
+//       }
+//     }
+//     .nav-right {
+//       display: flex;
+//       align-items: center;
+//       button {
+//         height: $nav-height - 20px;
+//         font-weight: 700;
+//         padding: 0 10px;
+//         font-family: $font;
+//         background-color: transparent;
+//         transition-duration: .2s;
+//         &:hover {
+//           transform: scale(1.15);
+//           color: $text-pink;
+//         }
+//         &.router-link-exact-active {
+//           transform: scale(1.15);
+//           color: $text-pink;
+//         }
+//       }
+//       .icon {
+//         height: $nav-height - 20px;
+//         display: flex;
+//         align-items: center;
+//         justify-content: center;
+//         transition-duration: .2s;
+//         min-width: 40px;
+//         &:hover {
+//           transform: scale(1.15) rotate(15deg);
+//           color: $text-pink;
+//         }
+//         &:visited {
+//           color: $text-dark;
+//           &:hover {
+//             color: $text-pink;
+//           }
+//         }
+//       }
+//     }
+//     @media (max-width: $breakpoint-small) {
+//       flex-direction: column;
+//       height: auto;
+//       justify-content: space-around;
+//       padding: 0;
+//       width: calc(100% - (8px * 2));
+//       .nav-left {
+//         height: 50px;
+//         .title {
+//           &:hover {
+//             svg {
+//               transform: none;
+//             }
+//           }
+//           &.router-link-exact-active {
+//             svg {
+//               transform: scale(1.75) rotate(15deg);
+//             }
+//           }
+//         }
+//       }
+//       .nav-right {
+//         max-width: 95%;
+//         overflow-x: auto;
+//         button {
+//           &:hover {
+//             transform: none;
+//             color: $text-dark;
+//           }
+//           &.router-link-active {
+//             transform: scale(1.15);
+//             color: $text-pink;
+//           }
+//         }
+//         .icon {
+//           &:hover {
+//             transform: none;
+//             color: $text-dark;
+//           }
+//           &:visited {
+//             color: $text-dark;
+//             &:hover {
+//               color: $text-pink;
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
 </style>
---
+++ src/components/JobCard.vue
@@ -0,0 +1,88 @@
+<template>
+<div id="job-card">
+  <div class="row">
+    <img v-if='job.image' :src='job.image' alt="job.image">
+    <div class="job-info">
+      <h2 class="title">{{job.title}}</h2><span> at </span>
+       <h2 class="company"><a class="link" target="_blank" :href='job.companyWebsite'>{{job.company}}</a></h2>
+      <p><b>{{job.workDate}}</b> on <b>{{job.contractType}}</b></p>
+    </div>
+    <h3>Activities :</h3>
+    <ul class="job-activities">
+      <li v-for='activity in job.activities'>{{activity}}</li>
+    </ul>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class JobCard extends Vue {
+  @Prop() private job!: any;
+}
+</script>
+
+<style lang="scss">
+@import './../sass/mrpink.io';
+
+#jobs {
+  margin: 0 8px;
+  padding-top: 50px + (8px * 2);
+  @include sm {
+    padding-top: 80px + (8px * 2);
+    .row {
+      width: 100%;
+    }
+  }
+  .row {
+    margin: auto;
+    margin-bottom: 8px;
+    border-radius: 6px;
+    background: $grey-light;
+    padding: 1em 1em 0.5em 1em;
+    width: 80%;
+    @include sm {
+      width: calc(100% - (8px * 2) - 1em);
+    }
+    img {
+      width: 175px;
+      float: right;
+      @include sm {
+        float: none;
+        display: block;
+        margin: auto;
+      }
+    }
+    h3 {
+      margin: 0.75em 0 0.30em 0;
+    }
+    .job-info {
+      display: inline-block;
+      h2 {
+        margin-top: 0;
+        margin-bottom: 0.35em;
+        @include sm {
+          margin-bottom: 0;
+        }
+      }
+      .title {
+        display: inline-block;
+        @include sm {
+          margin-top: 0.35em;
+        }
+      }
+      span {
+        font-size: 1.25em;
+      }
+      .company {
+        display: inline-block;
+      }
+    }
+    .job-activities {
+      margin: 0;
+    }
+  }
+}
+</style>
