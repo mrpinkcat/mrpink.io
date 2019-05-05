a4f3ab5d498c7141a243cb376b615706fad262d5
Gatien Anizan <gatien.anizan@gmail.com>
2019-05-05 16:22:23 +0200

    Typo

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -12,7 +12,7 @@
         <p>I like new technologies and having a good time with friends. My passion is divided between my love for technology and my thirst for creativity.</p>
         <p>My favorite time pass is coding, playing video games and getting lost on Reddit. I know Javascript, Typescipt, Pyhton, Swift, mongodb, nodejs and more, but my favorite environement is the Javascript. When I play video games I love playing to Factorio, Hacknet, Battlefield or League of Legend with my friends.</p>
         <p>In 2014 I learned to code. I preferred coding rather than learning my classes, that's why today I got my BAC (with honors) with a two years delay.</p>
-        <p>Since that day I've been learning new technologies. Actualy I'm on <b>vue.js</b>, I love this framework. It's much more intuitive than Angular (for example), and the coding team is mutch more dovted to the task and that see! This website is coding on vue.js with vue-ui and Typescipt.</p>
+        <p>Since that day I've been learning new technologies. Actualy I'm on <b>vue.js</b>, I love this framework. It's much more intuitive than Angular (for example), and the coding team is mutch more dovted to the task and that see! This website is coding on vue.js with vue-ui and Typescript.</p>
       </div>
     </div>
   </div>
