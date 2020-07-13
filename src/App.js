7f9a70f81f3f3827d6f86c5ed986a777c3ca6928
Gatien Anizan <gatien.anizan@gmail.com>
2020-07-13 05:57:39 +0200

    New design with styled-components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import './App.scss';
-import Terminal from './components/terminal/terminal';
+import styled from 'styled-components';
+import meImage from './assets/me.png';
+
+// Définie les règles CSS de la dic principale avec styled-components
+const MainDiv = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+const Title = styled.h1`
+  margin: 15px 0;
+  color: #FFF;
+  text-shadow: 0px 0px 2px rgba(10, 255, 10, .8);
+  text-align: center;
+`;
+
+const Image = styled.img`
+  height: 200px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-center">MrPink.dev</h1>
-      <Terminal />
-    </div>
+    <MainDiv>
+      <Title>MrPink.dev</Title>
+      <Image src={meImage} alt="Me" />
+    </MainDiv>
   );
 }
 
