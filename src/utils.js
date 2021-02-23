9002bae54d96549632dcbaeb0a1457bd78d52934
Gatien Anizan <gatien.anizan@gmail.com>
2021-02-23 11:30:04 +0100

    fix(age): no longer NaN age on firefox

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 const actualAge = () => {
-  const birthDate = new Date('08-20-1998').getTime();
+  const birthDate = new Date(903564000000).getTime();
   const timestamp = new Date().getTime();
   const age = (timestamp - birthDate) / 31556952000; // 31556952000 = 1 year
   return age.toString().slice(0, 2);
